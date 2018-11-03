ce42d90c4ddcdef05a65873855d3aceffd47bbaf
sambhav <sambhav.reddy@gmail.com>
2018-11-03 15:52:30 -0700

    files

--- serve.py
+++ serve.py
@@ -1,15 +1,17 @@
 import http.server
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import socketserver
-import os
 
 PORT = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
-Handler.extensions_map['.m3u8'] = 'vnd.apple.mpegURL'
-Handler.extensions_map['.ts'] = 'video/MP2T'
-Handler.extensions_map['.mp4'] = 'video/mp4'
+
+Handler.extensions_map[".m3u8"]="vnd.apple.mpegURL"
+Handler.extensions_map[".ts"]="video/MP2T"
+Handler.extensions_map[".mp4"]="video/mp4"
 
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
-print("Serving at port", PORT)
+
+print("serving at port", PORT)
 httpd.serve_forever()
