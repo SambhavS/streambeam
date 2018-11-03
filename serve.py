0e222b27e9f77de0750c1a4aa659ea3fc43665a6
sambhav <sambhav.reddy@gmail.com>
2018-11-03 16:54:55 -0700

    im done

--- serve.py
+++ serve.py
@@ -1,17 +1,14 @@
-import http.server
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import socketserver
+from flask import Flask, send_file, render_template
+app = Flask(__name__)
+mime_d = {"m3u8" : "vnd.apple.mpegURL", "ts" : "video/MP2T", "mp4": "video/mp4"}
 
-PORT = 8000
+@app.route('/index/')
+def main():
+    return render_template("index.html")
 
-Handler = http.server.SimpleHTTPRequestHandler
-
-Handler.extensions_map[".m3u8"]="vnd.apple.mpegURL"
-Handler.extensions_map[".ts"]="video/MP2T"
-Handler.extensions_map[".mp4"]="video/mp4"
+@app.route('/index/<fname>')
+def stuffy(fname):
+	return send_file(fname, mimetype=mime_d[fname.split(".")[-1]])
 
 
-httpd = socketserver.TCPServer(("", PORT), Handler)
-
-print("serving at port", PORT)
-httpd.serve_forever()
+#ffmpeg -i in.mkv -c:v h264 -flags +cgop -g 30 -hls_time 1 output.m3u8 -hls_list_size 0
